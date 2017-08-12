1a2559b29546d644b74822c1d551f1bb0f6cb029
savespace <savespace@outlook.com>
2017-08-12 08:33:15 +0200

    upd

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function drawTree (height){
 	for (i = 0 ; i <= height ; i++) {
 		var star = '';
-		for (j = 0 ; j <= i - 1 ; j++){
+		for (j = 0 ; j <= i ; j++){
 			star += '*';
 		}
 		console.log(star);
